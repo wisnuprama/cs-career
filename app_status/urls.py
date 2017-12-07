072f12fe45ad08a473bf70093001e84257556eac
Wisnu Pramadhitya <wisnuprama14@gmail.com>
2017-12-07 23:14:05 +0700

    app status urls

--- app_status/urls.py
+++ app_status/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import url
 import app_status.views as View
 
 urlpatterns = [
-    url(r'private-api/get/status/$', View.get, name='api-status-get'),
-    url(r'private-api/get/status/$', View.get, name='api-status-post'),
-    url(r'private-api/get/status/$', View.get, name='api-status-delete'),
+    url(r'get/status/$', View.get, name='get-status'),
+    url(r'post/status/$', View.get, name='post-status'),
+    url(r'delete/status/$', View.get, name='delete-status'),
     # url(r'private-api/get/status/$', View.get, name='status-get'),
 ]
