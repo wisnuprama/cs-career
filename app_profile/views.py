bae348635bdb164ffd822f91f49e23256810472e
Wisnu Pramadhitya Ramadhan <wisnuprama014@gmail.com>
2017-12-13 03:44:40 +0700

    conflict views profile

--- app_profile/views.py
+++ app_profile/views.py
@@ -1,14 +1,13 @@
-from app_profile import utils as profile_utils
 from django.shortcuts import render
-import app_auth.utils as auth_utils
+
+from app_profile import utils as profile_utils
 
 # Create your views here.
 response = {}
 
+
 def index(request, data):
-
     data_user = request.session['user_login']
-
     data['user_riwayat'] = profile_utils.get_query_user_history(data_user['npm'])
 
 
