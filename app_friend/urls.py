2d46f991166139949e5ee23b73d51ad955d86f90
Ario Seto <ario.seto99@gmail.com>
2017-12-12 15:55:20 +0700

    add views app_friend

--- app_friend/urls.py
+++ app_friend/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url
+from app_friend import views
 
 urlpatterns = [
-
+	url(r'^get-friend-candidate/$', views.get_all_friend_candidate, name='get-all-friend-candidate'),
 ]
