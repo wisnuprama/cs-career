289f8d6713ed6e141f2639c580da6204d62fa579
Wisnu Pramadhitya Ramadhan <wisnuprama014@gmail.com>
2017-12-12 23:23:08 +0700

    edit friend html

--- app_friend/urls.py
+++ app_friend/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from app_friend import views
 
 urlpatterns = [
-	url(r'^get-friend-candidate/$', views.get_all_friend_candidate, name='get-all-friend-candidate'),
+    url(r'^get-friend-candidate/$', views.get_all_friend_candidate, name='get-all-friend-candidate'),
+    url(r'^post-new-friend/$', views.post_new_friend, name='post-new-friend'),
 ]
--- app_friend/utils.py
+++ app_friend/utils.py
@@ -2,6 +2,7 @@ from .serializers import FriendshipSerializer
 from .models import Friendship
 from app_auth import utils as auth_utils
 
+
 def get_number_of_friend(user):
     return Friendship.objects.filter(user1=user).count()
 
@@ -10,16 +11,19 @@ def serialize_friendship(friendship):
     from core.utils import serialize_instance
     return serialize_instance(FriendshipSerializer, friendship)
 
+
 def get_user_friends(user):
-	return Friendship.objects.filter(user1=user)       
-       
+    return Friendship.objects.filter(user1=user)
+
+
 def insert_new_friend_to_database(user, user2npm):
-	user2 = auth_utils.get_or_create_user(npm=user2npm)
-	friendship = Friendship(user1=user, user2=user2)
-	friendship.save()
-	return friendship
+    user2 = auth_utils.get_or_create_user(npm=user2npm)
+    friendship = Friendship(user1=user, user2=user2)
+    friendship.save()
+    return friendship
+
 
 def get_all_mahasiswa_except(user):
-	query_user = auth_utils.get_except_user_queryset(npm=user.npm)
-	result = [auth_utils.serialize_user(q) for q in query_user]
-	return result
+    query_user = auth_utils.get_except_user_queryset(npm=user.npm)
+    result = [auth_utils.serialize_user(q) for q in query_user]
+    return result
